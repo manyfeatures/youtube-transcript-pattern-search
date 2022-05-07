5bdc054964c29ca5121e9718976c1c08c4f96837
manyfeatures <shakurs@mail.ru>
2022-05-07 19:20:43 +0300

    contend loading

--- WebDriver.py
+++ WebDriver.py
@@ -4,13 +4,47 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
 
 class WebDriver:
-    def __init__(self, channel_videos_url, web_driver_path, options=[]):
+    def __init__(self, channel_videos_url, web_driver_path, options_list=None):
         self.channel_url = channel_videos_url
-        self._options = init_options(options)
+        self._options = self.init_options(options_list)
         self._service = Service(executable_path=web_driver_path)
+        # only Firefox currently implemented
+        self.driver = webdriver.Firefox(service=self._service, options=self.options)
+        self.content_xpath = '//*[@id="contents"]'
+        self.timeout = 10
 
     def init_options(self, options_list):
         self.options = Options()
-        self.options.add_argument(*options_list)
+        if options_list:
+            self.options.add_argument(*options_list)
+
+    def open_page(self):
+        self.driver.get(self.channel_url)
+
+    def ensure_content_loaded(self):
+        try:
+            myElem = WebDriverWait(self.driver, self.timeout).until(EC.presence_of_element_located((By.XPATH, self.content_xpath)))
+            print()
+        except TimeoutException:
+            print
+            "Loading took too much time!"
+
+
+    def scroll_to_end(self):
+        """Scroll until the end is reached to get all content"""
+        self.ensure_content_loaded()
+
+        element = self.driver.find_element_by_xpath(self.content_xpath)  # the element you want to scroll to
+        prev_page_pos = 0
+        while True:
+            self.driver.find_element_by_tag_name('body').send_keys(Keys.END)
+            new_page_pos = str(self.driver.execute_script('return window.pageYOffset;'))
+            if prev_page_pos == new_page_pos:
+                break
+            prev_page_pos = new_page_pos
+            time.sleep(1)
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Search of words time steps in video/videos transcripts from the given YouTube channel
+from WebDriver import *
+
 def main():
+    channel_videos_url = "https://www.youtube.com/c/MadHighlights/videos"
+    web_driver_path = "./geckodriver"
+    driver = WebDriver(channel_videos_url, web_driver_path)
+    driver.open_page()
+    driver.scroll_to_end()
 
 if __name__ == "__main__":
     main()
